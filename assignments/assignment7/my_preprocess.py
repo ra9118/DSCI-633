37992a39ab5a7f1ccb0e81fb3736cdde0e0c3d8c
Rana Alrubaye <55769961+ra9118@users.noreply.github.com>
2020-10-10 14:09:50 -0400

    Hw7

--- assignments/assignment7/my_preprocess.py
+++ assignments/assignment7/my_preprocess.py
@@ -1,38 +1,41 @@
-import numpy as np
-from scipy.linalg import svd
+import my_preprocess
+import pandas as pd
+from collections import Counter
+from sklearn.tree import DecisionTreeClassifier
 
-def pca(X, n_components = 5):
-    #  Use svd to perform PCA on X
-    #  Inputs:
-    #     X: input matrix
-    #     n_components: number of principal components to keep
-    #  Output:
-    #     X_pca: output matrix of n_components columns (numpy.array)
-    #  Write your own code below:
-    U, s, Vh = svd(X)
-    return X_pca
+##################################################
 
-def normalize(X, norm="Min-Max", axis = 1):
-    #  Inputs:
-    #     X: input matrix
-    #     norm = {"L1", "L2", "Min-Max", "Standard_Score"}
-    #     axis = 0: normalize rows
-    #     axis = 1: normalize columns
-    #  Output:
-    #     X_norm: normalized matrix (numpy.array)
-    #  Write your own code below:
-    X_array = np.asarray(X)
-    return X_norm
+if __name__ == "__main__":
+    # Load training data
+    data_train = pd.read_csv("../data/Iris_train.csv")
+    # Separate independent variables and dependent variables
+    independent = ["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm"]
+    X = data_train[independent]
+    y = data_train["Species"]
+    # Preprocess (train)
+    X_norm = my_preprocess.normalize(X)
+    principal_components = my_preprocess.pca(X_norm, n_components=2)
+    X_pca = X_norm.dot(principal_components)
+    sample = my_preprocess.stratified_sampling(y, ratio=0.5, replace=False)
 
-def stratified_sampling(y, ratio, replacement = True):
-    #  Inputs:
-    #     y: class labels
-    #     0 < ratio < 1: number of samples = len(y) * ratio
-    #     replacement = True: sample with replacement
-    #     replacement = False: sample without replacement
-    #  Output:
-    #     sample: indices of stratified sampled points
-    #                    (ratio is the same across each class)
-    #  Write your own code below:
-    y_array = np.asarray(y)
-    return sample
+    X_sample = X_pca[sample]
+    y_sample = y[sample].to_numpy()
+    print(X_pca)
+    print(Counter(y_sample))
+    print(Counter(y))
+    # Fit model
+    clf = DecisionTreeClassifier()
+    clf.fit(X_sample, y_sample)
+    # Load testing data
+    data_test = pd.read_csv("../data/Iris_test.csv")
+    X_test = data_test[independent]
+    # Preprocess (test)
+    X_test_norm = my_preprocess.normalize(X_test)
+    X_test_pca = X_test_norm.dot(principal_components)
+    # Predict
+    predictions = clf.predict(X_test_pca)
+    # Output predictions on test data
+    print(predictions)
+
+
+
