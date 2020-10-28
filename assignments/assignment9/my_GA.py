a6c7e9418117349ba4340bccb313b90618ded39e
Rana Alrubaye <55769961+ra9118@users.noreply.github.com>
2020-10-28 00:16:23 -0400

    HW 9

--- assignments/assignment9/my_GA.py
+++ assignments/assignment9/my_GA.py
@@ -70,12 +70,12 @@ class my_GA:
             indices = [i for i in range(len(self.data_y))]
             np.random.shuffle(indices)
             size = int(np.ceil(len(self.data_y) / float(self.crossval_fold)))
-            objs_crossval = None
+            objs_crossval = []
             for fold in range(self.crossval_fold):
                 start = int(fold * size)
                 end = start + size
-                test_indices = indices["write your own code"]
-                train_indices = indices["write your own code"] + indices["write your own code"]
+                test_indices = indices[start:end]
+                train_indices = indices[0:start] + indices[end:]
                 X_train = self.data_X.loc[train_indices]
                 X_train.index = range(len(X_train))
                 X_test = self.data_X.loc[test_indices]
@@ -85,17 +85,20 @@ class my_GA:
                 y_test = self.data_y.loc[test_indices]
                 y_test.index = range(len(y_test))
                 clf.fit(X_train, y_train)
-                predictions = "write your own code"
-                pred_proba = "write your own code"
-                actuals = "write your own code"
+                predictions = clf.predict(X_test)
+                pred_proba = clf.predict_proba(X_test)
+                actuals = y_test
                 objs = np.array(self.obj_func(predictions, actuals, pred_proba))
-                if type(objs_crossval) == type(None):
-                    objs_crossval = "write your own code"
-                else:
-                    objs_crossval += "write your own code"
+                #prec = objs[0]
+                #rec = objs[0]
+                # if type(objs_crossval) == type(None):
+                #     objs_crossval = objs[0] #2.0 * prec * rec / (prec + rec)
+                # else:
+                #     objs_crossval += objs[0] #(2.0 * prec * rec / (prec + rec))
+                objs_crossval.append(objs[0])
 
-            objs_crossval = objs_crossval / float(len(self.data_y))
-            self.evaluated[decision] = objs_crossval
+            #objs_crossval = objs_crossval / float(len(self.data_y))
+            self.evaluated[decision] = tuple(objs_crossval)
         return self.evaluated[decision]
 
     def is_better(self, a, b):
@@ -108,7 +111,12 @@ class my_GA:
         obj_a = self.evaluate(a)
         obj_b = self.evaluate(b)
         # write your own code below
-        if "write your own code":
+        atLeastOne = False
+        for i in range(len(obj_a)):
+            if(obj_a[i] > obj_b[i]):
+                atLeastOne = True
+                break
+        if obj_a >= obj_b and atLeastOne == True:
             return 1
         else:
             return -1
@@ -126,7 +134,7 @@ class my_GA:
         modified = False
         for i in range(len(pf_best)):
             for j in range(len(pf_new)):
-                if "write your own code":
+                if self.is_better( pf_new[j],pf_best[i]) == 1:
                     pf_best[i] = pf_new[j]
                     pf_new.pop(j)
                     modified = True
@@ -135,7 +143,7 @@ class my_GA:
         for j in range(len(pf_new)):
             not_dominated = True
             for i in range(len(pf_best)):
-                if "write your own code":
+                if self.is_better( pf_best[i],pf_new[j]) == -1:
                     not_dominated = False
                     break
             if not_dominated:
@@ -184,8 +192,9 @@ class my_GA:
         # Write your own code below
         def cross(a, b):
             new_point = []
+            k = np.random.randint(len(a))
             for i in range(len(a)):
-                if "write your own code":
+                if i < k: # we assume k is middle of stream
                     new_point.append(a[i])
                 else:
                     new_point.append(b[i])
