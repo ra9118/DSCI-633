37a35c6ff91e3df70c2508679df242aa4c7a70fd
Rana Alrubaye <55769961+ra9118@users.noreply.github.com>
2020-11-20 22:29:55 -0500

    use hashing

--- assignments/project/project.py
+++ assignments/project/project.py
@@ -7,6 +7,10 @@ import pandas as pd
 import time
 from TextNLP import  TextNLP
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn import preprocessing
+
+min_max_scaler = preprocessing.MinMaxScaler()
 
 textNLP = TextNLP()
 
@@ -14,27 +18,21 @@ class my_model():
 
     def fit(self, X, y):
         print( "=============== Training the model ( takes up to 5 minutes )====================")
-        [processed_docs, X_clean] = textNLP.pre_pro_Cols(X)
-        [dictionary, corpus_tfidf, bow_corpus] = textNLP.tf_idf(processed_docs)
-        self._lda_model = textNLP.lda(corpus_tfidf, dictionary)
-        X_train = textNLP.apply_lda(self._lda_model, bow_corpus, X_clean)
+        X_train = textNLP.pre_pro_Cols(X)
         [X_train_balance, y_balance] = textNLP.balanceData(X_train,y)
+        X_norm = min_max_scaler.fit_transform(X_train_balance)
+
         # Create Decision Tree classifer object
-        self.clf = DecisionTreeClassifier()
-        print("(Step 6 of 6)  Train Decision Tree Classifer")
-        self.clf.fit(X_train_balance, y_balance)
+        self.clf = SGDClassifier()
+        self.clf.fit(X_norm, y_balance)
 
 
     def predict(self, X):
         print("=============== Testing the model ====================")
         # remember to apply the same preprocessing in fit() on test data before making predictions
-        [processed_docs, X_clean] = textNLP.pre_pro_Cols(X)
-        [dictionary, corpus_tfidf, bow_corpus] = textNLP.tf_idf(processed_docs)
-        print("(Step 4 of 6)  using existing {} LDA Topics ....".format(textNLP._numberOfTopics))
-        #self._lda_model = textNLP.lda(corpus_tfidf, dictionary)
-        X_test = textNLP.apply_lda(self._lda_model, bow_corpus, X_clean)
-        print("(Step 6 of 6)  predictions using Decision Tree Classifer")
-        return self.clf.predict(X_test)
+        X_test = textNLP.pre_pro_Cols(X)
+        X_norm = min_max_scaler.fit_transform(X_test)
+        return self.clf.predict(X_norm)
 
 
 if __name__ == "__main__":
