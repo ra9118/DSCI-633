6e00c243f217abc966a7e2ec03885c575e02262b
Rana Alrubaye <55769961+ra9118@users.noreply.github.com>
2020-11-20 22:29:33 -0500

    use HashingVectorizer

--- assignments/project/TextNLP.py
+++ assignments/project/TextNLP.py
@@ -1,32 +1,26 @@
 
 import pandas as pd
-import numpy as np
-import string
 
-from gensim.utils import lemmatize
-from gensim.parsing.preprocessing import remove_stopwords
-from gensim import corpora, models
+
 from sklearn.utils import resample
-
-# remove  non alphabetic characters.
-remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
-
+from sklearn.feature_extraction.text import TfidfVectorizer, HashingVectorizer
 
 class TextNLP:
 
      def __init__(self,
                   pp_colums = ['title','location','description','requirements'],
-                  outputCol ='fraudulent', 
-                  numberOfTopics = 5, 
+                  outputCol ='fraudulent',
+                  numberOfTopics = 5,
                   upSampling = False):
-         
+
         self._pp_colums = pp_colums
         self._numberOfTopics = numberOfTopics
         self._outputCol = outputCol
         self._upSampling = upSampling
+        self.vectorizer = None
 
      def balanceData(self,X,Y):
-         
+
         print("(Step 5(B) of 6) balancing the data")
         data = X
         data["fraudulent"] = Y
@@ -82,103 +76,37 @@ class TextNLP:
              textData.append(text)
 
          documents = pd.DataFrame(textData, columns=['headline_text'])
-         processed_docs = documents['headline_text'].map(self.textPreProcessing)
-
-         return [processed_docs, X_clean]
 
 
-     def tf_idf(self,processed_docs):
+         # create the transform
+         if self.vectorizer == None:
+             self.vectorizer = HashingVectorizer(n_features=self._numberOfTopics)
+             # self.vectorizer = TfidfVectorizer(stop_words='english', norm='l2', use_idf=False, smooth_idf=False)
 
-         print("(Step 2 of 6) Building Bag of Words ....")
-         dictionary = corpora.Dictionary(processed_docs)
-         # count = 0
-         # for k, v in dictionary.iteritems():
-         #     print(k, v)
-         #     count += 1
-         #     if count > 10:
-         #         break
+             # encode document
+             vectors = self.vectorizer.fit_transform(documents["headline_text"])
+         else:
+             vectors = self.vectorizer.transform(documents["headline_text"])
 
-         #print("Building Bag of Words and TF-IDF dataset ....")
-         dictionary.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
-         bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
+         # summarize encoded vector
 
-         # TF-IDF
-         print("(Step 3 of 6) Apply TF-IDF")
-         tfidf = models.TfidfModel(bow_corpus)
-         corpus_tfidf = tfidf[bow_corpus]
+         for idx in range(self._numberOfTopics):
+             X_clean["Topic_{}".format(idx)] = 0
 
-         return [dictionary, corpus_tfidf, bow_corpus]
+         #print(vectors.shape)
+         row_index = 0
+         for vector in vectors:
+             #print(vector)
+             for idx in range(len(vector.data)):
+                 #print(vector.data[idx])
+                 X_clean.iloc[row_index, self._shiftCols + idx] = vector.data[idx]
+                 #idx = idx + 1
 
-     def lda(self,corpus_tfidf,dictionary):
+             row_index = row_index + 1
 
-         print("(Step 4 of 6)  Start generate {} LDA Topics ....".format(self._numberOfTopics))
-         lda_model = models.LdaMulticore(corpus_tfidf,
-                                         num_topics=self._numberOfTopics,
-                                         id2word=dictionary,
-                                         passes=2,
-                                         workers=2)
-
-         # Print the LDA topics
-         for idx, topic in lda_model.print_topics():
-             print('Topic: {} \nWords: {}'.format(idx, topic))
-
-         return lda_model
-
-     def apply_lda(self,lda_model,bow_corpus,X):
-
-         print("(Step 5 of 6)  Find LDA topic for every document ....")
-         #Add needed columns
-         for idx, topic in lda_model.print_topics():
-             X["Topic_{}".format(idx)] = np.nan
-
-         # Find LDA for every document
-
-         for row_index in range(len(bow_corpus)):
-             for index, score in sorted(lda_model[bow_corpus[row_index]], key=lambda tup: -1 * tup[1]):
-                 # print("\nScore: {}\t \nTopic: {}".format(score, lda_model.print_topic(index, 10)))
-                 idx = 0
-                 for w in lda_model.print_topic(index, self._numberOfTopics).split("+"):
-                     X.iloc[row_index, self._shiftCols + idx] = float(w.split("*")[0])
-                     idx = idx + 1
-
-         return X
+         #print(vector.toarray())
 
 
-     def textPreProcessing(self,text):
-        #print("============ textPreProcessing2 ==========")
-
-        # Removing stop words frequent words
-        text_no_stopwords = remove_stopwords(text)
-        #print(text_no_stopwords)
-
-        # remove  punctuation
-        text_no_punct = text_no_stopwords.translate(remove_punct_dict)
-        #print(text_no_punct)
-
-        #Lemmatizer
-        token_lemmatized = [wd.decode('utf-8').split('/')[0] for wd in lemmatize(text_no_punct)]
-        #print(token_lemmatized)
-
-        # remove  non alphabetic characters.
-        tokens_alphabetic = [word.lower() for word in token_lemmatized if word.isalpha()]
-        #print(tokens_alphabetic)
+         return X_clean
 
 
-        return tokens_alphabetic
-
-
-
-## Test code
-def main():
-    textNLP = TextNLP()
-    str= "Create a named mock of the request type from this builder. The same builder can be called to create multiple mocks.";
-    print(str)
-    newText= textNLP.textPreProcessing(str)
-    print(newText)
-    str= "Creates mock object of given class or interface. and See examples in javadoc for Mockito class";
-    print(str)
-    newText= textNLP.textPreProcessing(str)
-    print(newText)
-
-
-if __name__ == '__main__':main()
